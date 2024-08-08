a9c3e96cb32fc87272ffb739f361cb8d0ca5b2c6
Hedi M. Sharif <hedi.mohammed@dit.gov.krd>
2024-08-08 18:12:50 +0300

    enhancement(stepper): making spacing better

--- src/components/stepper-layout/index.tsx
+++ src/components/stepper-layout/index.tsx
@@ -6,7 +6,7 @@ export const StepperLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-row items-center h-screen p-8">
       <Sidebar />
-      <main className="flex flex-1 flex-col items-center justify-center h-full px-24">
+      <main className="flex flex-1 flex-col items-center justify-center h-full px-24 py-12">
         <div className="flex-1 w-full">{children}</div>
         <Action />
       </main>
